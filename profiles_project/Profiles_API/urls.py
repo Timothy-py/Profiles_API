017b546e4de88639557803368aaf13185337939d
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-11-15 12:04:58 +0100

    Added user profile serializer, view set and created the router

--- profiles_project/Profiles_API/urls.py
+++ profiles_project/Profiles_API/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello_viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 
 urlpatterns = [
